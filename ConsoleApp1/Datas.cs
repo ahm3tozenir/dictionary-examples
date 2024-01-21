a56754a31b98f7a6da4a426d76e8141a653f6a97
ahm3tozenir <ozenirahm3t@gmail.com>
2024-01-21 19:38:09 +0300

    initial

---
+++ ConsoleApp1/Datas.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Example;
+public class Datas
+{
+    public static string PersonDataJson = "[{\n  \"identity\": \"12432423\",\n  \"userName\": \"dfairbourn0\",\n  \"password\": \"lZ0@2\",\n  \"isActive\": true,\n  \"firstName\": \"Deeyn\",\n  \"lastName\": \"Fairbourn\",\n  \"ssn\": \"644-64-2957\",\n  \"salary\": 212\n}, {\n  \"identity\": \"23432223\",\n  \"userName\": \"rkerwen1\",\n  \"password\": \"zP0{Wu\",\n  \"isActive\": true,\n  \"firstName\": \"Rhona\",\n  \"lastName\": \"Dugall\",\n  \"ssn\": \"405-96-0875\",\n  \"salary\": 212\n}, {\n  \"identity\": \"5672423\",\n  \"userName\": \"crollett2\",\n  \"password\": \"cW4,,t\\\"@\",\n  \"isActive\": false,\n  \"firstName\": \"Chaunce\",\n  \"lastName\": \"Rollett\",\n  \"ssn\": \"816-92-3028\",\n  \"salary\": 210\n}]";
+
+    public static string StudentDataJson = "[{\n  \"identity\": \"12452423\",\n  \"studentNumber\": \"123\", \n  \"userName\": \"rklus0\",\n  \"password\": \"eR1_\",\n  \"isActive\": false,\n  \"absenteeism\": 15,\n  \"marks\": 85\n}, {\n  \"identity\": \"72432423\",\n  \"studentNumber\": \"345\",\n  \"userName\": \"asimonot1\",\n  \"password\": \"dX3~?\",\n  \"isActive\": false,\n  \"absenteeism\": 5,\n  \"marks\": 90\n}, {\n  \"identity\": \"92441423\",\n  \"studentNumber\": \"678\",\n  \"userName\": \"wivanishev2\",\n  \"password\": \"aO8?woSG\",\n  \"isActive\": true,\n  \"absenteeism\": 15,\n  \"marks\": 86\n}, {\n  \"identity\": \"11443423\",\n  \"studentNumber\": \"898\",\n  \"userName\": \"mdaybell3\",\n  \"password\": \"lD4|=q\",\n  \"isActive\": true,\n  \"absenteeism\": 14,\n  \"marks\": 83\n}]";
+
+    public static string JobberDataJson = "[{\n  \"identity\": \"12452421\",\n  \"licenseNumber\": \"1223\",\n  \"userName\": \"cmoy0\",\n  \"password\": \"bN7@}Xq\",\n  \"isActive\": false,\n  \"workArea\": \"Enejit Airport\"\n}, {\n  \"identity\": \"47452423\",\n  \"licenseNumber\": \"4455\",\n  \"userName\": \"fbettenay1\",\n  \"password\": \"pD4!QeM\",\n  \"isActive\": false,\n  \"workArea\": \"Perugia San Francesco d'Assisi Umbria International Airport\"\n}]";
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,109 @@
+﻿using Example;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+var personalUsers = JsonConvert.DeserializeObject<IList<Personal>>(Datas.PersonDataJson);
+var studentUsers = JsonConvert.DeserializeObject<IList<Student>>(Datas.StudentDataJson) ;
+var jobberUsers = JsonConvert.DeserializeObject<IList<Jobber>>(Datas.JobberDataJson);
+IDictionary<string,IList<string>> indexes =new Dictionary<string,IList<string>>();
+IDictionary<string, IUser> fastList = new Dictionary<string, IUser>();
+
+fastList.AddToDictionary(personalUsers.Select(user=>(user as IUser)).ToList(),indexes);
+fastList.AddToDictionary(studentUsers.Select(user=>(user as IUser)).ToList(),indexes);
+fastList.AddToDictionary(jobberUsers.Select(user=>(user as IUser)).ToList(),indexes);
+
+var findAllByFirstName = FindByIndex("Deeyn");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByFirstName));
+var findAllByLasttName = FindByIndex("Fairbourn");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByLasttName));
+var findAllByUserName = FindByIndex("dfairbourn0");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByUserName));
+var findAllByIdentity = FindByIndex("12432423");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByIdentity));
+var findAllBySSN = FindByIndex("644-64-2957");
+Console.WriteLine(JsonConvert.SerializeObject(findAllBySSN));
+var findAllByStudentNumber = FindByIndex("345");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByStudentNumber));
+var findAllByAbsenteeism = FindByIndex("14");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByAbsenteeism));
+var findAllByMarks = FindByIndex("83");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByMarks));
+var findAllByLicenseNumber = FindByIndex("1223");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByLicenseNumber));
+var findAllByWorkArea = FindByIndex("Perugia San Francesco d'Assisi Umbria International Airport");
+Console.WriteLine(JsonConvert.SerializeObject(findAllByWorkArea));
+
+
+Console.ReadKey();
+
+IList<IUser>? FindByIndex(string search)
+{
+    if (indexes.ContainsKey(search))
+    {
+        var findedKeys=indexes[search];
+        return findedKeys.Select(key => fastList[key]).ToList();
+    }
+    return null;
+}
+public static class MicrosoftExtensions
+{
+
+    public static void AddToDictionary<TKey,TValue>(
+        this IDictionary<TKey, TValue> values, 
+        IList<TValue> users,
+        IDictionary<TKey,IList<TKey>> indexes
+        )
+        where TValue: IUser
+        where TKey: notnull
+    {
+        TKey castToKey(object key)
+        {
+            return (TKey)key;
+        };
+        void addIndex(object findKeyObject, TKey dataKey)
+        {
+            TKey findKey= castToKey(findKeyObject);
+            if (indexes.ContainsKey(findKey))
+            {
+                indexes[findKey].Add(dataKey);
+            }
+            else
+            {
+                indexes.Add(findKey, new List<TKey>() { dataKey });
+            }
+        };
+        users?.ToList().ForEach(user =>
+        {
+            TKey key = castToKey(user.UserName);
+            values.Add(key, user);
+            addIndex(user.FirstName, key);
+            addIndex(user.LastName, key);
+            addIndex(user.Identity, key);
+            addIndex(user.Password, key);
+            var personal = user.CastTo<IPersonal>();
+            if (personal!=null){
+                addIndex(personal.SSN, key);                
+            }
+            var student = user.CastTo<IStudent>();
+            if (student!=null){
+                addIndex(student.Absenteeism.ToString(), key);
+                addIndex(student.Marks.ToString(), key);
+                addIndex(student.StudentNumber.ToString(), key);
+            }
+            var jobber = user.CastTo<IJobber>();
+            if (jobber!=null){
+                addIndex(jobber.LicenseNumber, key);
+                addIndex(jobber.WorkArea, key);
+            }
+        });
+    }
+    public static TObject? CastTo<TObject>(this object value)
+        where TObject: class
+    {
+        if (value is TObject)
+        {
+            return value as TObject;
+        }
+        return null;
+    }
+}
